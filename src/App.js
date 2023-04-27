99965ade13828a3f866da5348b823a09182242a7
Def <deferenseo@gmail.com>
2023-04-27 17:58:08 +0300

    hook up macro-css/fonts, finished header block

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 
 
-
 function App() {
     return (
-        <div className="wrapper">
+        <div className="wrapper clear">
+            <header className="d-flex justify-between align-center p-40">
+                <div className="d-flex align-center">
+                    <img width={40} height={40} src="/img/logo.png" alt="" />
+                    <div className="headerInfo">
+                        <h3 className="text-uppercase">React Sneakers</h3>
+                        <p>Магазин лучших кроссовок</p>
+                    </div>
+                </div>
+                <ul className="headerRight d-flex">
+                    <li className="mr-30">
+                        <img width={18} height={18} src="/img/cart.svg" alt="" />
+                        <span>1205 rub</span>
+                    </li>
+                    <li>
+                        <img width={18} height={18} src="/img/user.svg" alt="" />
+                    </li>
+                </ul>
+            </header>
 
+            <div className="content p-40">
+                <h1>Все кроссовки</h1>
+                ...
+            </div>
         </div>
     );
 }
