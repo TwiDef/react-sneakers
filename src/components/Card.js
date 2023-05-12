1eb6ad5d7806270686f5e3339d5d4c273e3f5324
Def <deferenseo@gmail.com>
2023-05-12 21:20:52 +0300

    render Card component with array of data

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,15 @@
-function Card() {
+function Card(props) {
     return (
         <div className="card">
             <div className="favorite">
                 <img src="/img/heart-unliked.svg" alt="unliked" />
             </div>
-            <img width={133} height={112} src="/img/sneakers/1.jpg" alt="sneaker" />
-            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <img width={133} height={112} src={props.imageUrl} alt="sneaker" />
+            <h5>{props.title}</h5>
             <div className="d-flex justify-between align-center">
                 <div className="d-flex flex-column">
                     <span>Цена:</span>
-                    <b>12 990 руб.</b>
+                    <b>{props.price} руб.</b>
                 </div>
                 <button className="button">
                     <img width={11} height={11} src="/img/plus.svg" alt="plus" />
